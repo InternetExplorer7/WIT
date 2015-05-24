ad85bbffbc4681cf812de24942e5f6797bf95813
InternetExplorer7 <themaster231436@gmail.com>
2015-05-24 01:30:39 -0700

    static

--- index.js
+++ index.js
@@ -7,7 +7,7 @@
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.use(express.static('public'));
+app.use(app.static('public'));
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
